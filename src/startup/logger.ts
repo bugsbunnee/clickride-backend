5eec74acb6c7fe62a973db927995d8ffa4b11ca7
marcel <marcel.chukwuma00@gmail.com>
2025-01-09 10:02:48 +0100

    removing-log

--- src/startup/logger.ts
+++ src/startup/logger.ts
@@ -20,6 +20,10 @@ if (process.env.NODE_ENV === 'production') {
     // };
 
     // logger.add(new winston.transports.MongoDB(transportOptions));
+
+    logger.add(new winston.transports.Console({ format: winston.format.combine(winston.format.colorize(), winston.format.json()), }));
+    logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }));
+    logger.add(new winston.transports.File({ filename: 'combined.log' }));
 } else {
     logger.add(new winston.transports.Console({ format: winston.format.combine(winston.format.colorize(), winston.format.json()), }));
     logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }));
